44758799573d7246041596349a552e5f041be06f
Evan <evfredericksen@gmail.com>
2021-08-24 16:54:23 -0400

    add lexicon path

--- srabuilder/core.py
+++ srabuilder/core.py
@@ -9,6 +9,7 @@ from io import BytesIO
 from zipfile import ZipFile
 import urllib.request
 from pathlib import Path
+import shutil
 
 from dragonfly import RecognitionObserver, get_engine
 from dragonfly.log import setup_log
@@ -37,12 +38,15 @@ def download_model(write_dir):
         my_zip_file.extractall(write_dir)
     print('Done!')
 
-def setup_engine(silence_timeout=500, models_dir=MODELS_DIR, expected_error_rate_threshold=0.05):
+def setup_engine(silence_timeout=500, models_dir=MODELS_DIR, expected_error_rate_threshold=0.05, lexicon_path=None):
     # use abspath for model dir, this may change with app freezing
     current_dir = os.path.dirname(os.path.abspath(__file__))
     model_dir = os.path.join(models_dir, "kaldi_model")
     if not os.path.isdir(model_dir):
         download_model(models_dir)
+    if lexicon_path is not None:
+        dst = os.path.join(model_dir, "user_lexicon.txt")
+        shutil.copyfile(lexicon_path, dst)
     # Set any configuration options here as keyword arguments.
     engine = get_engine(
         "kaldi",
