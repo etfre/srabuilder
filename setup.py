3058a592267bb1f82bd9c6d68519ddeaf4f1bc8f
Evan <evfredericksen@gmail.com>
2023-03-16 19:24:41 -0400

    test

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 from setuptools import find_packages
 import distutils.text_file
+import sys
 from pathlib import Path
 from typing import List
 
@@ -15,7 +16,7 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     # install_requires=_parse_requirements('requirements.txt'),
-    install_requires="""dfly-breathe, git+https://github.com/tiaxrulesall/kaldi-active-grammar/releases/download/mac-build-hotfix/kaldi_active_grammar-3.1.0-py2.py3-none-macosx_13_0_arm64.whl; sys_platform == 'darwin'"""
+    install_requires=["dfly-breathe", "git+https://github.com/tiaxrulesall/kaldi-active-grammar/releases/download/mac-build-hotfix/kaldi_active_grammar-3.1.0-py2.py3-none-macosx_13_0_arm64.whl; sys_platform == 'darwin'"]
     license='MIT',
     long_description='placeholder',
 )
